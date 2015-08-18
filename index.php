0fc49afe612e08528343be1c017307c5e101388d
Jeri Elizabeth <jeri.elizabeth@gmail.com>
2015-08-18 14:22:27 -0400

    adding video information for live booknotes site

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 
 <div id="callout">
     <div id="video-intro">
-    <!--
-        <iframe src="https://player.vimeo.com/video/69445362?portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-    -->
-    <!--    
         <video controls autoplay muted> 
-            <source src="files/original/26835c8683f6afcef087fe7aa5a8c34e.mp4" type="video/mp4">
+            <source src="files/original/6cc36550f716edfd0433aa661c183fd5.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video> 
-    -->
     </div>
 </div>
 
@@ -39,7 +34,7 @@
 
 <div id="recent-items">
     <h2><?php echo __('Featured Items'); ?></h2>
-    <?php echo random_featured_items(5); ?>
+    <?php echo random_featured_items(4); ?>
     <p class="view-items-link"><a class="action" href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
 </div><!--end recent-items -->
 
