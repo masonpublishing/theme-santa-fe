91422ac8a13df1d3e6be9b9cf8498af5c9875963
Jeri Elizabeth <jeri.elizabeth@gmail.com>
2015-09-04 14:50:57 -0400

    added footer logos and styles

--- common/footer.php
+++ common/footer.php
@@ -2,18 +2,24 @@
 
 <div id="footer" role="contentinfo">
 
-
-    <div id="custom-footer-text">
-        <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
-        <p><?php echo $footerText; ?></p>
-        <?php endif; ?>
-        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-            <p><?php echo $copyright; ?></p>
-        <?php endif; ?>
+    <div id="footer-info">
+        <div id="custom-footer-text">
+            <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
+            <p><?php echo $footerText; ?></p>
+            <?php endif; ?>
+            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+                <p><?php echo $copyright; ?></p>
+            <?php endif; ?>
+        </div>
+        <div id="footer-image">
+            <img src="<?php echo img('libraries-logo.png'); ?>">
+        </div>
     </div>
-
+    <div id="footer-attribution">
+        <img src="<?php echo img('publishing-logo-tall.png'); ?>">
+        <p>Hosted by the <a href="http://publishing.gmu.edu">Mason Publishing Group</a>.</p>
+    </div>
     <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
-
     <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 
 
